d3de0f1b67500180e0081ff618a39ecb86056293
David <davidpprg@gmail.com>
2025-10-29 13:58:42 +0100

    feat(replace_numbers): add replace numbers method

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -3,4 +3,4 @@ import textutils.core as c
 def replace_numbers():
     input_text = "There are 2 apples and 5 oranges."
     expected_output = "There are two apples and five oranges."
-    assert c.replace_numbers(input_text) == expected_output 
+    assert c.replace_numbers(input_text) == expected_output

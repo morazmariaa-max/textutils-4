fd03407b563800555e010c9df2df690c4d4ef98c
jakobkohr <jakob.kohrgruber@alumni.esade.edu>
2025-11-03 14:06:35 +0100

    feat(is_anagram) imports

---
+++ src/textutils/core.py
@@ -0,0 +1,6 @@
+import textutils.core as c
+
+def is_anagram(word1, word2):
+    word1 = word1.replace(" ", "").lower()
+    word2 = word2.replace(" ", "").lower()
+    return sorted(word1) == sorted(word2)

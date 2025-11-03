6d283327d5bef8a0e4617889dd751d53f618ed04
Kika <mariaangelica.morazamora@alumni.esade.edu>
2025-11-03 13:44:43 +0100

    final coding line

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,8 +1,12 @@
 import pytest
 from textutils.core import strip_accents
+
 @pytest.mark.parametrize("inp,exp", [
     ("canción Café mañana Árbol ñandú", "cancion Cafe manana Arbol nandu"),
     ("ÁÉÍÓÚ áéíóú", "AEIOU aeiou"),
     ("", ""),
     (None, ""),
 ])
+
+def test_strip_accents_basic(inp, exp):
+    assert strip_accents(inp) == exp

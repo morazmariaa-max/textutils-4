4dc3ec9a66b072514373935d3ada9ba7b523698c
jakobkohr <jakob.kohrgruber@alumni.esade.edu>
2025-11-03 14:13:06 +0100

    feat(is_anagram) working function implem entation

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,6 +1,14 @@
 import textutils.core as c
 
-def is_anagram(word1, word2):
-    word1 = word1.replace(" ", "").lower()
-    word2 = word2.replace(" ", "").lower()
-    return sorted(word1) == sorted(word2)
+def is_anagram(str1: str, str2: str) -> bool:
+    """
+    Check if two strings are anagrams (ignoring spaces and case).
+    Example:
+        is_anagram("amor", "roma") -> True
+        is_anagram("Listen", "Silent") -> True
+    """
+    
+    s1 = str1.replace(" ", "").lower()
+    s2 = str2.replace(" ", "").lower()
+
+    return sorted(s1) == sorted(s2)

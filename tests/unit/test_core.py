92b9b8bcab51d8f519f373949045a9cfd45e8f08
warrenliu87 <warrenliu87@gmail.com>
2025-11-03 13:42:05 +0100

    Warren's changes

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,2 +1,8 @@
 import pytest
 from textutils.core import strip_accents
+@pytest.mark.parametrize("inp,exp", [
+    ("canción Café mañana Árbol ñandú", "cancion Cafe manana Arbol nandu"),
+    ("ÁÉÍÓÚ áéíóú", "AEIOU aeiou"),
+    ("", ""),
+    (None, ""),
+])
